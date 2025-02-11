3dc190066ebeb50748b7cf7d9cd821bf764ec71b
kazuya kawaguchi <kawakazu80@gmail.com>
2025-02-11 18:11:59 +0900

    fix: add entry

--- knip.config.ts
+++ knip.config.ts
@@ -1,6 +1,6 @@
 import type { KnipConfig } from 'knip'
 
 export default {
-  entry: ['src/index.ts', 'eslint.config.ts'],
+  entry: ['src/index.ts', 'bench/index.mjs', 'eslint.config.ts'],
   ignoreDependencies: ['lint-staged']
 } satisfies KnipConfig
