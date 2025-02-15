c23e4c73e5444fffd93a4a1c0d9e0462c436d4c4
kazuya kawaguchi <kawakazu80@gmail.com>
2025-02-16 01:10:47 +0900

    feat: support convenient API parse

---
+++ src/parse.test.ts
@@ -0,0 +1,51 @@
+import { expect, test } from 'vitest'
+import { parse } from './parse.js'
+
+import type { ArgOptions } from './resolver'
+
+const options = {
+  help: {
+    type: 'boolean',
+    short: 'h'
+  },
+  version: {
+    type: 'boolean',
+    short: 'v'
+  },
+  port: {
+    type: 'number',
+    short: 'p',
+    default: 8080
+  },
+  mode: {
+    type: 'string',
+    short: 'm'
+  },
+  host: {
+    type: 'string',
+    short: 'o',
+    required: true
+  }
+} as const satisfies ArgOptions
+
+test('parse', () => {
+  const args = [
+    'dev',
+    '-p9131',
+    '--host=example.com',
+    'foo',
+    '-m=production',
+    '-h',
+    '--version',
+    'bar'
+  ]
+  const { values, positionals } = parse(args, { options })
+  expect(values).toEqual({
+    port: 9131,
+    host: 'example.com',
+    mode: 'production',
+    help: true,
+    version: true
+  })
+  expect(positionals).toEqual(['dev', 'foo', 'bar'])
+})
