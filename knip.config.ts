e7cf9b542599d3d45f9662a8608a328317220d7e
kazuya kawaguchi <kawakazu80@gmail.com>
2025-03-30 21:21:51 +0900

    fix: isolatedDeclarations error

--- knip.config.ts
+++ knip.config.ts
@@ -1,7 +1,9 @@
 import type { KnipConfig } from 'knip'
 
-export default {
+const config: KnipConfig = {
   entry: ['src/index.ts', 'playground/bun/index.ts', 'eslint.config.ts'],
   ignore: ['playground/deno/**', 'bench/mitata.js'],
   ignoreDependencies: ['lint-staged', 'mitata', 'deno']
-} satisfies KnipConfig
+}
+
+export default config
