0648ecd79942338d5d5ec9ca0ed659be5f5cff87
kazuya kawaguchi <kawakazu80@gmail.com>
2025-04-21 11:59:45 +0900

    chore: add jsr exports linting

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'tsdown'
+import { jsrExportsLint } from 'tsdown-jsr-exports-lint'
 
 const config: ReturnType<typeof defineConfig> = defineConfig({
   entry: ['src/index.ts', 'src/parser.ts', 'src/resolver.ts'],
   outDir: 'lib',
-  dts: true
+  clean: true,
+  dts: true,
+  onSuccess: jsrExportsLint()
 })
 
 export default config
